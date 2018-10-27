2b17147168171a6bfc5e1428ad393dc73e390e1e
Amupoti <amupoti@gmail.com>
2018-10-27 20:15:31 +0200

    Add module to parse calendar from RDM

---
+++ supermanager-rdm-parser/src/main/java/org/amupoti/supermanager/parser/rdm/RdmTeam.java
@@ -0,0 +1,9 @@
+package org.amupoti.supermanager.parser.rdm;
+
+/**
+ * Created by amupoti on 27/10/2018.
+ */
+public enum RdmTeam {
+
+    AND, BAS, BRE, BUR, CAN, EST, FCB, FUE, GBC, GCA, JOV, MAN, MUR, OBR, RMA, UNI, VAL, ZAR
+}
