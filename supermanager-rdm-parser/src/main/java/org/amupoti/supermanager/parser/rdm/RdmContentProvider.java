066beb262c0116707f9ada756dc82d339b9c044a
Amupoti <amupoti@gmail.com>
2018-10-27 20:15:31 +0200

    Add module to parse calendar from RDM

---
+++ supermanager-rdm-parser/src/main/java/org/amupoti/supermanager/parser/rdm/RdmContentProvider.java
@@ -0,0 +1,32 @@
+package org.amupoti.supermanager.parser.rdm;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Created by amupoti on 28/08/2017.
+ */
+@Service
+public class RdmContentProvider {
+
+    private final static String TEAM_URL = "http://www.rincondelmanager.com/smgr/equipo.php?eq=%s";
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @PostConstruct
+    public void init() {
+        restTemplate.getMessageConverters().add(new FormHttpMessageConverter());
+    }
+
+
+    protected String getTeamPage(RdmTeam rdmTeam) {
+        String url = String.format(TEAM_URL, rdmTeam.name());
+        ResponseEntity<String> exchange = restTemplate.getForEntity(url, String.class);
+        return exchange.toString();
+    }
+}
---
+++ supermanager-rdm-parser/src/main/java/org/amupoti/supermanager/parser/rdm/config/RdmConfiguration.java
@@ -0,0 +1,27 @@
+package org.amupoti.supermanager.parser.rdm.config;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by Marcel on 25/09/2015.
+ */
+@Configuration
+@ComponentScan("org.amupoti.supermanager.parser.rdm")
+public class RdmConfiguration {
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        CloseableHttpClient httpClient = HttpClientBuilder
+                .create()
+                .build();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        RestTemplate restTemplate = new RestTemplate(factory);
+        return restTemplate;
+    }
+}
