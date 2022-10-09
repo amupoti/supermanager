ebf6f12c1b185f748bbc1061b9c7741a01d24b20
amupoti <amupoti@gmail.com>
2022-10-09 17:01:56 +0200

    Enabling retry after failure when retriving data from other sites

--- supermanager-acb-parser/src/main/java/org/amupoti/supermanager/parser/acb/SmContentProvider.java
+++ supermanager-acb-parser/src/main/java/org/amupoti/supermanager/parser/acb/SmContentProvider.java
@@ -4,22 +4,23 @@ import lombok.extern.slf4j.Slf4j;
 import org.amupoti.supermanager.parser.acb.beans.SmTeam;
 import org.amupoti.supermanager.parser.acb.dto.LoginRequest;
 import org.amupoti.supermanager.parser.acb.dto.LoginResponse;
-import org.amupoti.supermanager.parser.acb.dto.SigninRequest;
 import org.amupoti.supermanager.parser.acb.dto.SigninResponse;
 import org.amupoti.supermanager.parser.acb.exception.ErrorCode;
+import org.amupoti.supermanager.parser.acb.exception.InfrastructureException;
 import org.amupoti.supermanager.parser.acb.exception.SmException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.*;
 import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by amupoti on 28/08/2017.
@@ -28,13 +29,13 @@ import java.util.Map;
 @Slf4j
 public class SmContentProvider {
 
+    public static final String LOGIN_URL = "https://admin-supermanager.acb.com/oauth/V2/open/accounttoken/getTokens";
+    public static final String PRE_LOGIN_URL = "https://id.acb.com/api/signIn";
     private static final String SUPERMANAGER_HOME_URL = "http://supermanager.acb.com/index/identificar";
+    public static final String ACTIVE_COMPETITION = SUPERMANAGER_HOME_URL;
     private static final String URL_TEAM_LIST = "https://supermanager.acb.com/api/basic/userteam/all";
     private static final String MARKET_PAGE = "https://supermanager.acb.com/api/basic/player?_filters={fields}&_page=1&_perPage=30";
     private static final String MARKET_PAGE_FIELDS = "[{\"field\":\"competition.idCompetition\",\"value\":1,\"operator\":\"=\",\"condition\":\"AND\"},{\"field\":\"edition.isActive\",\"value\":true,\"operator\":\"=\",\"condition\":\"AND\"}]";
-    public static final String ACTIVE_COMPETITION = SUPERMANAGER_HOME_URL;
-    public static final String LOGIN_URL = "https://admin-supermanager.acb.com/oauth/V2/open/accounttoken/getTokens";
-    public static final String PRE_LOGIN_URL = "https://id.acb.com/api/signIn";
     private String competition;
 
     @Autowired
@@ -78,15 +79,18 @@ public class SmContentProvider {
         return checkUserLogin(user, password);
     }
 
-    private LoginResponse checkUserLogin(String user, String password) {
+    @Retryable(maxAttempts = 3, value = InfrastructureException.class,
+            backoff = @Backoff(delay = 1000, multiplier = 2))
+    public LoginResponse checkUserLogin(String user, String password) {
 
         try {
             ResponseEntity<SigninResponse> signinResponse = restTemplate.postForEntity(PRE_LOGIN_URL, buildSigninRequest(user, password), SigninResponse.class);
             ResponseEntity<LoginResponse> responseEntity = restTemplate.postForEntity(LOGIN_URL, addAuthenticationDetails(signinResponse.getBody()), LoginResponse.class);
             return responseEntity.getBody();
-        } catch (Exception e) {
+        } catch (HttpClientErrorException e) {
             throw new SmException(ErrorCode.INVALID_CREDENTIALS, e);
-
+        } catch (Exception e) {
+            throw new InfrastructureException("Ha ocurrido un problema al intentar recuperar la información", e);
         }
     }
 
@@ -94,9 +98,9 @@ public class SmContentProvider {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-        MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
-        map.add("username",user);
-        map.add("password",password);
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("username", user);
+        map.add("password", password);
 
         return new HttpEntity<>(map, headers);
 
