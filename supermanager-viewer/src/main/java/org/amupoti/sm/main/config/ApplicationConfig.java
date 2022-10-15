0364f7468053a5b4f7ed16c715da32d31bc477f7
amupoti <amupoti@gmail.com>
2022-10-15 10:27:13 +0200

    Use retry handler to avoid stale connections

--- supermanager-viewer/src/main/java/org/amupoti/sm/main/config/ApplicationConfig.java
+++ supermanager-viewer/src/main/java/org/amupoti/sm/main/config/ApplicationConfig.java
@@ -1,10 +1,13 @@
 package org.amupoti.sm.main.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.amupoti.supermanager.parser.acb.SmContentParser;
 import org.amupoti.supermanager.parser.acb.SmContentProvider;
 import org.amupoti.supermanager.parser.acb.teams.SMUserTeamService;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -21,7 +24,15 @@ import static org.amupoti.supermanager.parser.acb.SmContentProvider.ACTIVE_COMPE
 @Configuration
 @PropertySource("classpath:urls.properties")
 @ComponentScan("org.amupoti.supermanager.parser.rdm")
+@Slf4j
 public class ApplicationConfig {
+
+    private static final int CONNECT_TIMEOUT = 30 * 1000;
+    private static final int REQUEST_TIMEOUT = 30 * 1000;
+    private static final int SOCKET_TIMEOUT = 60 * 1000;
+
+    @Autowired
+    CloseableHttpClient httpClient;
 
     @Bean
     public PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
@@ -29,17 +40,38 @@ public class ApplicationConfig {
     }
 
     @Bean
-    public RestTemplate getRestTemplate() {
-        CloseableHttpClient httpClient = HttpClientBuilder
-                .create()
-                .disableRedirectHandling()
-                .disableCookieManagement()
+    public RestTemplate restTemplate() {
+        return new RestTemplate(clientHttpRequestFactory());
+    }
+
+    @Bean
+    public HttpComponentsClientHttpRequestFactory clientHttpRequestFactory() {
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setHttpClient(httpClient());
+        return clientHttpRequestFactory;
+    }
+
+    @Bean
+    public CloseableHttpClient httpClient() {
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(REQUEST_TIMEOUT)
+                .setConnectTimeout(CONNECT_TIMEOUT)
+                .setSocketTimeout(SOCKET_TIMEOUT).build();
+
+        return HttpClients.custom()
+                .setDefaultRequestConfig(requestConfig)
+                .setRetryHandler((exception, executionCount, context) -> {
+                    if (executionCount > 3) {
+                        log.warn("Maximum tries reached for client http pool ");
+                        return false;
+                    }
+                    if (exception instanceof org.apache.http.NoHttpResponseException) {
+                        log.warn("No response from server on " + executionCount + " call");
+                        return true;
+                    }
+                    return false;
+                })
                 .build();
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
-        factory.setConnectionRequestTimeout(10000);
-        factory.setConnectTimeout(10000);
-        factory.setReadTimeout(10000);
-        return new RestTemplate(factory);
     }
 
     @Bean
